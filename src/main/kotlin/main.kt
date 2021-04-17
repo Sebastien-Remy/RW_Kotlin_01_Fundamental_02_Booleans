4e26dd8b89b15ff3aaf21407a666d5998015a6b4
Sebastien REMY <seb.remy@me.com>
2021-04-17 13:53:08 +0200

    operators

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,9 +2,9 @@ fun main() {
     val isProgrammingAwesome = true
     println("Is programming awesome? $isProgrammingAwesome")
 
-    val myAge = 50
-    val isMageDrivingAge = myAge >= 18
-    println ("My age is $myAge, driving license age: $isMageDrivingAge")
+    var myAge = 50
+    var isDrivingAge = myAge >= 18
+    println ("My age is $myAge, driving license age: $isDrivingAge")
 
     val yearOffBirth = 1971
     val iBornOnTwentyFirstCentury = yearOffBirth > 2000
@@ -12,7 +12,6 @@ fun main() {
 
     val myName = "REMY"
     val otherName = "REMY"
-//    val otherName = "CHAPO"
     val isSameName = myName == otherName
     println ("$myName and $otherName are same name? $isSameName")
 
@@ -30,9 +29,17 @@ fun main() {
     var passwordIsSecure = pasword.length >= 10
     println ("Password is secure (1)? $passwordIsSecure")
 
-    // Challende 2
+    // Challenge 2
     // Password same name length
     passwordIsSecure = pasword.length == myName.length
     println ("Password is secure (2)? $passwordIsSecure")
+
+    // Operators
+    myAge = 16
+    val country = "USA"
+    isDrivingAge = (myAge >= 18 && country == "France") || (country == "USA" && myAge >= 16)
+    println ("At $myAge years old is legal to drive in $country? $isDrivingAge")
+
+
 }
 
