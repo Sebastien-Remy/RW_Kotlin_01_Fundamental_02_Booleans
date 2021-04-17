761e56590d1352248631d4fe9956834acb8af96b
Sebastien REMY <seb.remy@me.com>
2021-04-17 13:59:34 +0200

    If Else

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -35,11 +35,21 @@ fun main() {
     println ("Password is secure (2)? $passwordIsSecure")
 
     // Operators
-    myAge = 16
+    myAge = 17
     val country = "USA"
     isDrivingAge = (myAge >= 18 && country == "France") || (country == "USA" && myAge >= 16)
     println ("At $myAge years old is legal to drive in $country? $isDrivingAge")
 
+    // If Else
+    if (isDrivingAge) {
+        println("You can drive!")
+    } else {
+        println("You've got a bit more time to go!")
+    }
+
+    if(myAge >=18) println("I'm an adult") else println("I'm still underaged!")
+
+    println (if (myAge >=18) "I'm an adult" else "I'm still underaged!")
 
 }
 
